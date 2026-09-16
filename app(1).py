9e080ef661262f658effce3e30782a75a52b2dbb
jansherkhan1998 <79219950+jansherkhan1998@users.noreply.github.com>
2026-09-16 22:00:34 +0500

    Update app(1).py

--- app(1).py
+++ app(1).py
@@ -4,7 +4,7 @@ from datetime import date
 
 import streamlit as st
 from google import genai
-from google.genai import types
+#from google.genai import types
 
 
 # ============================================================
