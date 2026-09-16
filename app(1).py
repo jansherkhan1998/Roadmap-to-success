7a6984ae0172932c982ea04ba4d27b0d9e315871
jansherkhan1998 <79219950+jansherkhan1998@users.noreply.github.com>
2026-09-16 22:32:59 +0500

    Update app(1).py

--- app(1).py
+++ app(1).py
@@ -84,7 +84,7 @@ def ask_gemini(prompt, json_mode=False):
   )
 
   response = client.models.generate_content(
-      model="gemini-2.5-flash", contents=prompt, config=config
+      model="gemini-3.6-flash", contents=prompt, config=config
   )
 
   return response.text
